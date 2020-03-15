ae05c62ef36647a74b14ea201272a344c2b0ff58
Ivon Saldivar <saldivar.ivon@ichsstudent.org>
2020-03-15 15:58:28 -0600

    Got my basic scripts working. Now to make it do something when you solve it

---
+++ Statues/Assets/Scripts/RotateStatueCW.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class RotateStatueCW : RotateStatue
+{
+
+    public override IEnumerator MoveWall(){
+        float startTime = Time.time;
+        float overTime = 0.2f;
+        float endTime = startTime + overTime;
+
+        float totalRotation = 90f;
+        float rotationProgress = 0;
+        float perLoop = 3.0f;
+
+        while (rotationProgress < totalRotation)
+        {
+            wall.transform.Rotate(0.0f, perLoop, 0.0f, Space.Self);
+            rotationProgress += perLoop;
+            yield return null;
+        }
+
+    }
+}
---
+++ Statues/Assets/Scripts/trigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+    public GameObject door;
+
+    void OnTriggerEnter(Collider colider){
+        door.transform.position += new Vector3(0, 4, 0);
+    }
+}
