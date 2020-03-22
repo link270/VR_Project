ce5559dfa8c22afa6cf25c57d0c9d296bfaf48ee
Ivon Saldvar <saldivar.ivon@ichsstudent.org>
2020-03-21 18:49:57 -0600

    Teleportation isn't working

---
+++ Statues/Assets/ResetTiles.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ResetTiles : MonoBehaviour
+{
+    public HoverButton lever;
+    
+    private Player player;
+    private GameObject vrcamera;
+    private GameObject[] tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Player.instance;
+        vrcamera = GameObject.Find("VRCamera");
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+        lever.onButtonDown.AddListener(OnbuttonDown);
+    }
+
+    private void OnbuttonDown(Hand hand){
+        StartCoroutine(ResetTilePuzzle());
+    }
+
+    private IEnumerator ResetTilePuzzle(){
+        Debug.Log(player.transform.position);
+        player.transform.position = gameObject.transform.position;
+        vrcamera.transform.position = gameObject.transform.position;
+        Debug.Log(gameObject.transform.position);
+        
+        foreach (GameObject tile in tiles){
+            //Debug.Log("Resetting " + tile.name);
+            tile.transform.position = new Vector3(tile.transform.position.x, 0f, tile.transform.position.z);
+        }
+        return null;
+    }
+
+}
