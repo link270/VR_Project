b5361da41709024d935cd1453dc1bd4f3fe71c4d
Ivon Saldvar <saldivar.ivon@ichsstudent.org>
2020-03-22 22:11:32 -0600

    Got pseudo-teleportion working!

--- Statues/Assets/ResetTiles.cs
+++ Statues/Assets/ResetTiles.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR.InteractionSystem;
-
+namespace Valve.VR.InteractionSystem
+{
 public class ResetTiles : MonoBehaviour
 {
     public HoverButton lever;
     
-    private Player player;
+    private  Player player;
     private GameObject vrcamera;
     private GameObject[] tiles;
 
@@ -15,26 +15,40 @@ public class ResetTiles : MonoBehaviour
     void Start()
     {
         player = Player.instance;
-        vrcamera = GameObject.Find("VRCamera");
+        //vrcamera = GameObject.Find("VRCamera");
         tiles = GameObject.FindGameObjectsWithTag("Tile");
-        lever.onButtonDown.AddListener(OnbuttonDown);
     }
 
-    private void OnbuttonDown(Hand hand){
+    public void OnLeverPressed(Hand hand){
         StartCoroutine(ResetTilePuzzle());
     }
 
     private IEnumerator ResetTilePuzzle(){
-        Debug.Log(player.transform.position);
-        player.transform.position = gameObject.transform.position;
-        vrcamera.transform.position = gameObject.transform.position;
-        Debug.Log(gameObject.transform.position);
+        //Debug.Log(player.transform.position);
+        // GameObject PlayerCamera = GameObject.Find("VRCamera");  //get the VRcamera object
+        // Debug.Log("Camera: " + PlayerCamera.name);
+        // Debug.Log("Player: " + player.name);
+        // Vector3 GlobalCameraPosition = PlayerCamera.transform.position;  //get the global position of VRcamera
+        // Vector3 GlobalPlayerPosition = player.transform.position;
+        // Vector3 GlobalOffsetCameraPlayer = new Vector3(GlobalCameraPosition.x - GlobalPlayerPosition.x, 0, GlobalCameraPosition.z - GlobalPlayerPosition.z);
+        // Vector3 newRigPosition = new Vector3(gameObject.transform.position.x - GlobalOffsetCameraPlayer.x, player.transform.position.y, gameObject.transform.position.z - GlobalOffsetCameraPlayer.z);
+        // player.transform.position = newRigPosition;
+            SteamVR_Fade.Start(Color.black, 0.2f);
+            yield return new WaitForSeconds(0.2f);
+            //player.transform.Rotate(Vector3.up, 90f);
+            Vector3 translation = gameObject.transform.position - player.transform.position;
+            player.transform.Translate(translation.x, translation.y, translation.z, Space.World);
+            SteamVR_Fade.Start(Color.clear, 0.2f);
+            yield return new WaitForSeconds(0.2f);
+            //vrcamera.transform.position = new Vector3(1,1,1);
+        //Debug.Log(gameObject.transform.position);
         
         foreach (GameObject tile in tiles){
             //Debug.Log("Resetting " + tile.name);
             tile.transform.position = new Vector3(tile.transform.position.x, 0f, tile.transform.position.z);
         }
-        return null;
+        yield return null;
     }
 
+}
 }
