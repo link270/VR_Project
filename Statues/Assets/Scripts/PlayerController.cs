f78340d427d9e56d8a5389f9f7f3ce0acd63c066
Ivon Saldvar <saldivar.ivon@ichsstudent.org>
2020-03-29 17:48:07 -0600

    Added statues,

--- Statues/Assets/Scripts/PlayerController.cs
+++ Statues/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
 {
     public SteamVR_Action_Vector2 input;
     public float speed = 3;
-
     private CharacterController characterController;
 
     private void Start()
@@ -19,10 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
 
         //System.Console.WriteLine("test");
-        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
+        if(input.axis.x != 0 && input.axis.y != 0){
+            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+            characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
+        }
     }
 }
---
+++ urban adVentuRe!/Assets/Scripts/RotateStatueCW.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class RotateStatueCW : RotateStatue
+{
+
+    public override IEnumerator Rotate(){
+
+        float startTime = Time.time;
+        float overTime = 0.2f;
+        float endTime = startTime + overTime;
+
+        float totalRotation = 90f;
+        float rotationProgress = 0;
+        float perLoop = 3.0f;
+
+        while (rotationProgress < totalRotation)
+        {
+            wall.transform.Rotate(0.0f, perLoop, 0.0f, Space.Self);
+            rotationProgress += perLoop;
+            wall.GetComponent<Orientation>().orientation += perLoop;
+            if(wall.GetComponent<Orientation>().orientation == 180) wall.GetComponent<Orientation>().orientation = 0;
+            yield return null;
+        }
+
+    }
+}
