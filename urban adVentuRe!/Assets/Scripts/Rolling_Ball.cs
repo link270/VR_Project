821e002b543ae6c340dd95e00b07cc6702fcb29a
Spencer Griffin <xcgriffs@gmail.com>
2020-04-02 17:16:50 -0600

    Ball puzzle now more compact, ball is not visible until in hallway.

--- urban adVentuRe!/Assets/Scripts/Rolling_Ball.cs
+++ urban adVentuRe!/Assets/Scripts/Rolling_Ball.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class Rolling_Ball : MonoBehaviour
 {
-    public Rigidbody ball;
+    //public Rigidbody ball;
+    public GameObject trapDoor;
+    public GameObject ball;
+
+    public int visibleDistance;
+
     public int angularVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        ball.maxAngularVelocity = angularVelocity;
+        ball.GetComponent<Rigidbody>().maxAngularVelocity = angularVelocity;
+        ball.GetComponent<MeshRenderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(ball.transform.position, trapDoor.transform.position) < visibleDistance){
+            ball.GetComponent<MeshRenderer>().enabled = true;
+        }else{
+            ball.GetComponent<MeshRenderer>().enabled = false;
+        }
     }
 }
